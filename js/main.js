6bf09092d4fc3879bff129bf29a105dcbec40fe7
Raghav Gandotra <raghavgandotra16@gmail.com>
2022-07-25 17:32:55 -0400

    refactor

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
+const RESULT_TYPES = {
+  TIE: "TIE",
+  WIN: "WIN",
+  LOSE: "LOSE",
+};
+
+let playerScore = 0;
+let computerScore = 0;
+const MAX_SCORE = 5;
+
 function computerPlay() {
   const NUM_CHOICES = 3;
   const randomVal = Math.floor(Math.random() * NUM_CHOICES);
@@ -8,13 +18,7 @@ function computerPlay() {
   } else return "SCISSORS";
 }
 
-const RESULT_TYPES = {
-  TIE: "TIE",
-  WIN: "WIN",
-  LOSE: "LOSE",
-};
-
-function playRound(playerSelection, computerSelection) {
+function computeOutcome(playerSelection, computerSelection) {
   playerSelection = playerSelection.toUpperCase();
 
   if (playerSelection === computerSelection) {
@@ -61,70 +65,72 @@ function outcomeDescriptionText(
   }
 }
 
+function displayOutcomeDescription(result, playerSelection, computerSelection) {
+  document.getElementById("outcome-description").textContent =
+    outcomeDescriptionText(result, playerSelection, computerSelection);
+}
+
 function displaySelections(playerSelection, computerSelection) {
   document.getElementById("player-selection").textContent = playerSelection;
   document.getElementById("computer-selection").textContent = computerSelection;
 }
 
-function game(playerSelection, computerSelection) {
-  displaySelections(playerSelection, computerSelection);
-
-  const result = playRound(playerSelection, computerSelection);
-  document.getElementById("outcome-description").textContent =
-    outcomeDescriptionText(result, playerSelection, computerSelection);
-
+function updateWinnerScore(result) {
   if (result === RESULT_TYPES.WIN) {
     playerScore = playerScore + 1;
   } else if (result === RESULT_TYPES.LOSE) {
     computerScore = computerScore + 1;
   }
+}
 
+function displayUpdatedScores() {
   document.getElementById("player-score").textContent = playerScore;
   document.getElementById("computer-score").textContent = computerScore;
+}
 
-  if (playerScore === MAX_SCORE || computerScore === MAX_SCORE) {
-    gameOver();
+function playRound(playerSelection, computerSelection) {
+  displaySelections(playerSelection, computerSelection);
+
+  const result = computeOutcome(playerSelection, computerSelection);
+
+  displayOutcomeDescription(result, playerSelection, computerSelection);
+
+  updateWinnerScore(result);
+
+  displayUpdatedScores();
+
+  const isGameOver = playerScore === MAX_SCORE || computerScore === MAX_SCORE;
+  if (isGameOver) {
+    displayGameOver();
   }
 }
 
-function gameOver() {
-  rockButton.disabled = true;
-  paperButton.disabled = true;
-  scissorsButton.disabled = true;
-
-  if (playerScore > computerScore) {
-    document.getElementById("game-over").textContent =
-      "The Player is the Winner! Reload the page to play again!";
-  } else {
-    document.getElementById("game-over").textContent =
-      "The Computer is the Winner! Reload the page to play again!";
-  }
+function disableButtons() {
+  const buttons = document.querySelectorAll(".selection-button");
+  buttons.forEach(function (button) {
+    button.disabled = true;
+  });
 }
 
-const rockButton = document.querySelector("#rock-selection-button");
-const paperButton = document.querySelector("#paper-selection-button");
-const scissorsButton = document.querySelector("#scissors-selection-button");
+function displayGameOver() {
+  disableButtons();
 
-let playerSelection = "";
-let computerSelection = "";
-let playerScore = 0;
-let computerScore = 0;
-const MAX_SCORE = 5;
+  const playerWin = playerScore > computerScore;
 
-rockButton.addEventListener("click", () => {
-  playerSelection = rockButton.dataset.selection;
-  computerSelection = computerPlay();
-  game(playerSelection, computerSelection);
-});
+  document.getElementById("game-over").textContent = `The ${
+    playerWin ? "Player" : "Computer"
+  } is the Winner! Reload the page to play again!`;
+}
 
-paperButton.addEventListener("click", () => {
-  playerSelection = paperButton.dataset.selection;
-  computerSelection = computerPlay();
-  game(playerSelection, computerSelection);
-});
+function addButtonClickListeners() {
+  const buttons = document.querySelectorAll(".selection-button");
+  buttons.forEach(function (button) {
+    button.addEventListener("click", () => {
+      const playerSelection = button.dataset.selection;
+      computerSelection = computerPlay();
+      playRound(playerSelection, computerSelection);
+    });
+  });
+}
 
-scissorsButton.addEventListener("click", () => {
-  playerSelection = scissorsButton.dataset.selection;
-  computerSelection = computerPlay();
-  game(playerSelection, computerSelection);
-});
+addButtonClickListeners();
